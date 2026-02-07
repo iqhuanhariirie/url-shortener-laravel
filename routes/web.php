38594d0250b2ebd67d0994e953f17be0b96e2bd7
iqhuanhariirie <iqhuanhariirie1173@gmail.com>
2026-02-07 17:58:27 +0800

    create a post and get route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UrlController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/urls', [UrlController::class, 'store'])->name('urls.store');
+Route::get('/{code}', [UrlController::class, 'redirect'])
+    ->where('code', '[A-Za-z0-9]{6}')
+    ->name('urls.redirect');
